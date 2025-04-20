dc72e6edd3f77f955e2209974860722dd9447130
Fathy Samir <01154857080fa@gmail.com.com>
2025-04-20 09:43:26 +0200

    fathysamir

--- resources/views/dashboard/register.blade.php
+++ resources/views/dashboard/register.blade.php
@@ -265,7 +265,7 @@
                 </div>
                 <div class="form-group">
                     <label for="inputPassword" class="sr-only">Password</label>
-                    <input pattern="[A-Za-z0-9]{50}" title="Password must be exactly 8 digits" type="password"
+                    <input pattern="[A-Za-z0-9]{50}" title="Password must be exactly 50 digits" type="password"
                         id="inputPassword" name="password" class="form-control form-control-lg"
                         placeholder="Password" required>
                     @if ($errors->has('password'))
