ef00584ffea70f5eda1a624652ba87156307c7b3
Fathy Samir <01154857080fa@gmail.com.com>
2025-07-14 12:43:25 +0300

    mmm

--- app/Http/Controllers/Dashboard/DocumentController.php
+++ app/Http/Controllers/Dashboard/DocumentController.php
@@ -206,8 +206,8 @@ class DocumentController extends ApiController
 
             // Access sourceId from response
             $sourceId = $data['sourceId'] ?? null;
-            $message  = 'Letters are normally issued on a specific date and has a reference number and subject.  It should be signed at the end and most likely start with “Dear Sir” or “Dear Sirs”.
-Based on the above, do you see that the uploaded document is a Letter? If yes answer with “Letter” and stop.
+            $message  = 'Letter is a formal written communication sent from one party to another, typically  issued on a specific date and containing a reference number and subject, and signed at the end by the sender.  Most likely its body starts with “Dear Sir” or “Dear Sirs”.
+Based on the above, do you see that the uploaded document is a Letter? If yes, answer with “Letter” and stop.
 If not, If not, and if you see that the document represents an e-mail message, answer with "e-mail" and stop.
 If not, and provided that we have the following list of document types: \n ';
             $x = ["email","e-mail" ,"letter"];
