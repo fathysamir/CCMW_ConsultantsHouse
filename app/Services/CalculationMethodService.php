a64a24aa1a83396080e4bad03cabdb35df5a4085
Fathy Samir <01154857080fa@gmail.com.com>
2025-11-02 14:12:07 +0200

    auth controller

--- app/Services/CalculationMethodService.php
+++ app/Services/CalculationMethodService.php
@@ -382,7 +382,7 @@ class CalculationMethodService
         $CompensabilityCalculation = CalculationMethod::where('project_id', $project_id)->where('key', 'CompensabilityCalculation')->first();
         $CompensabilityCalculation = $CompensabilityCalculation ? $CompensabilityCalculation->value : '2';
         if ($CompensabilityCalculation == '1') {
-
+           
             $window          = Window::findOrFail($window_id);
             $previous_window = Window::where('project_id', auth()->user()->current_project_id)
                 ->whereRaw('CAST(REGEXP_SUBSTR(no, "[0-9]+") AS UNSIGNED) < ?', [$window->no])
@@ -399,19 +399,18 @@ class CalculationMethodService
                 $result = $window->duration;
             }
         } elseif ($CompensabilityCalculation == '2') {
-
-            $milestones_IDs = Milestone::where('project_id', auth()->user()->current_project_id)->pluck('id')->toArray();
-            $date1          = $this->comp_date($project_id, $window_id, 'UPD', $milestones_IDs);
-            $date2          = $this->comp_date($project_id, $window_id, 'BUT');
-            $start    = Carbon::parse($date2);
-            $end      = Carbon::parse($date1);
-            $duration = $start->diffInDays($end, false);
-            $result   = $duration;
+           
+            $milestones_IDs      = Milestone::where('project_id', auth()->user()->current_project_id)->pluck('id')->toArray();
+            $date1               = $this->comp_date($project_id, $window_id, 'UPD', $milestones_IDs);
+            $date2               = $this->comp_date($project_id, $window_id, 'BUT');
+            $start               = Carbon::parse($date2);
+            $end                 = Carbon::parse($date1);
+            $duration            = $start->diffInDays($end, false);
+            $result              = $duration;
             dd($result);
-
             $fnExcusableOFLastMS = $this->fnExcusableOFLastMS($project_id, $window_id);
             if ($result > $fnExcusableOFLastMS) {
-
+               
                 $result = $fnExcusableOFLastMS;
             }
             $window          = Window::findOrFail($window_id);
@@ -420,7 +419,7 @@ class CalculationMethodService
                 ->orderByRaw('CAST(REGEXP_SUBSTR(no, "[0-9]+") AS UNSIGNED) DESC')
                 ->first();
             if ($previous_window) {
-
+                
                 $compensableTransfer = $this->compensableTransfer($project_id, $previous_window->id);
             } else {
                 $compensableTransfer = 0;
